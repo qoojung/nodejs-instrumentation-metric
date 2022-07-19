3fc7ba6fbcf94f825d08d2948a4bcb32754be191
Henry Chang <qoo392@gmail.com>
2022-07-19 15:21:55 +0800

    Change: fix port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { installMetricHandler, metricMiddleware } = require('./metric')();
 
 const app = express();
-const port = 3000;
+const port = process.env.port || 9898;
 app.use(metricMiddleware);
 
 app.get('/', (req, res) => {
